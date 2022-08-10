57af42e21f74d2215d903de53e6d101dd5301d0e
JedwardMook <jedwardmook@yahoo.com>
2022-08-09 20:02:30 -0500

    commit tree 9467d66caea4ec748c15ae01a3dae7453b2ec25f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,45 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        render json: user.items
+      else
+        render json: { error: "User not found"}, status: :not_found
+      end
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
   end
+
+  def show
+    if params[:id]
+      item = Item.find(params[:id])
+      if item
+        render json: item
+      end
+    end
+  end
+
+  def create
+    if params[:user_id]
+      item = Item.create(item_params)
+      render json: item, status: :created
+    end
+  end
+
+  private
+
+  def not_found_response
+    render json: { error: "Not found"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
 
 end
